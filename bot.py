ee9251e3a8296e8a19520145866d8f746cdab368
Ukenn <duola2002@gmail.com>
2022-01-21 08:33:55 +0800

    简化back_week_callback

--- bot.py
+++ bot.py
@@ -674,10 +674,8 @@ def collection_callback(call):
 def back_week_callback(call):
     day = int(call.data.split('|')[1])
     week_data = gender_week_message(day)
-    text = week_data['text']
-    markup = week_data['markup']
     bot.delete_message(chat_id=call.message.chat.id , message_id=call.message.message_id, timeout=20)
-    bot.send_message(chat_id=call.message.chat.id, text=text, parse_mode='Markdown', reply_markup=markup, timeout=20)
+    bot.send_message(chat_id=call.message.chat.id, text=week_data['text'], parse_mode='Markdown', reply_markup=week_data['markup'], timeout=20)
     bot.answer_callback_query(call.id)
 
 
