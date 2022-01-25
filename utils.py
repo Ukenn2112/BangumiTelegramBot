dabacc84a9919bc5730eec9ae4741359d9a78d87
Ukenn <duola2002@gmail.com>
2022-01-25 20:46:19 +0800

    缩减anilist的请求数据

--- utils.py
+++ utils.py
@@ -408,18 +408,8 @@ def anime_img(subject_id):
     query = '''
     query ($id: Int, $page: Int, $perPage: Int, $search: String) {
         Page (page: $page, perPage: $perPage) {
-            pageInfo {
-                total
-                currentPage
-                lastPage
-                hasNextPage
-                perPage
-            }
             media (id: $id, search: $search) {
                 id
-                title {
-                    romaji
-                }
             }
         }
     }
