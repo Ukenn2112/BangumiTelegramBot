175e3a0bb7ae634af6f01b6ad4ce621f9e559b5c
Ukenn <duola2002@gmail.com>
2022-01-27 22:45:19 +0800

    完善README并将summar和summary_call合并

--- README.md
+++ README.md
@@ -69,6 +69,47 @@ week - 查询当日/空格加数字查询每日放送
 search - 搜索条目
 ```
 
+# 文件树
+
+```txt
+BangumiTelegramBot   # 主目录
+│
+│  bgm_data.json     # Bangumi 用户密钥
+│  bot.py            # BangumiBot 主模块
+│  config.py         # 配置文件
+│  oauth.py          # Bangumi Oauth 绑定验证模块
+│  requirements.txt  # Python 所需依赖
+│  
+├─plugins  # 功能
+│  │  doing_page.py  # 查询 Bangumi 用户在看
+│  │  info.py        # 根据subjectId 返回对应条目信息
+│  │  my.py          # 查询 Bangumi 用户收藏统计
+│  │  search.py      # 搜索引导
+│  │  start.py       # 查询/绑定 Bangumi
+│  │  week.py        # 每日放送查询
+│  │  
+│  ├─callback  # 按钮查询
+│  │      collection.py      # 收藏
+│  │      letest_eps.py      # 已看最新
+│  │      now_do.py          # 在看详情
+│  │      rating_call.py     # 评分
+│  │      search_details.py  # 搜索详情
+│  │      summary_call.py    # 简介
+│  │      week_back.py       # 每日放送查询返回
+│  │      
+│  └─inline  # 消息框内联查询
+│          public.py  # 公共内联搜索
+│          sender.py  # 私聊搜索或者在任何位置搜索前使用@内联搜索
+│          
+├─templates  # Oauth 认证提示页面模板
+│      error.html     # 绑定出错
+│      verified.html  # 重复验证
+│      
+└─utils  # 通用
+        api.py       # API 调用
+        converts.py  # 数据转换
+```
+
 # 其它
 
 - 这是我第一次写的Python项目，没系统学习过Python，许多细节上可能没有规范还请大佬们多多包涵，如您有更好的解决方式欢迎提交PR，谢谢^_^
--- plugins/callback/summary_call.py
+++ plugins/callback/summary_call.py
@@ -1,5 +1,9 @@
 """简介页"""
-from plugins.summary import grnder_summary_message
+import telebot
+from typing import Optional
+
+from utils.api import get_subject_info
+from utils.converts import subject_type_to_emoji
 
 
 def callback(call, bot):
@@ -22,3 +26,21 @@ def callback(call, bot):
                               message_id=call.message.message_id,
                               reply_markup=summary_data['markup'])
     bot.answer_callback_query(call.id)
+
+
+def grnder_summary_message(subject_id, week_day: Optional[str] = None):
+    subject_info = get_subject_info(subject_id)
+    text = {f"{subject_type_to_emoji(subject_info['type'])} *{subject_info['name_cn']}*\n"
+            f"{subject_info['name']}\n\n"
+            f"*➤ 简介：*\n"
+            f"{subject_info['summary']}\n"
+            f"\n📖 [详情](https://bgm.tv/subject/{subject_id})"
+            f"\n💬 [吐槽箱](https://bgm.tv/subject/{subject_id}/comments)"}
+    markup = telebot.types.InlineKeyboardMarkup()
+    if week_day != 0:
+        markup.add(telebot.types.InlineKeyboardButton(
+            text='返回', callback_data=f'search_details|week|{subject_id}|{week_day}|1'))
+    else:
+        markup.add(telebot.types.InlineKeyboardButton(
+            text='返回', callback_data=f'search_details|search|{subject_id}|0|1'))
+    return {'text': text, 'markup': markup}
