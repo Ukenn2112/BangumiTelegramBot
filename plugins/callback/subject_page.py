0994eecfa88b2dc6d1bc82d2965daaacb512e62d
rin304 <rin304@163.com>
2022-03-11 13:51:32 +0800

    NSFW条目显示图标

--- plugins/callback/subject_page.py
+++ plugins/callback/subject_page.py
@@ -127,6 +127,8 @@ def gander_page_text(subject_id, user_collection=None, subject_info=None) -> str
         text += f"*BGM ID：*`{subject_id}` | {user_collection['status']['name']}"
     else:
         text += f"*BGM ID：*`{subject_id}`"
+    if subject_info['nsfw']:
+        text += "🔞"
     text += "\n"
     if subject_info and 'rating' in subject_info and 'score' in subject_info['rating']:
         text += f"*➤ BGM 平均评分：*`{subject_info['rating']['score']}`🌟 " \
