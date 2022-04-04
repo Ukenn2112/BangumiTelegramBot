a950c5fbb86f3b54fa4a2c57c5e17da4fb91c14e
Ukenn <duola2002@gmail.com>
2022-04-04 12:13:08 +0800

    完善解绑功能

--- plugins/unbind.py
+++ plugins/unbind.py
@@ -1,11 +1,14 @@
 """查询/绑定 Bangumi"""
 
-from utils.api import user_data_delete
+from utils.api import data_seek_get, user_data_delete
 
 
 def send(message, bot):
     if message.text == "/unbind 确认":
-        user_data_delete(message.from_user.id)
-        bot.send_message(message.chat.id, text='已解绑QAQ, 期待下次与你见面！', parse_mode='Markdown', timeout=20)
+        if data_seek_get(message.from_user.id):
+            user_data_delete(message.from_user.id)
+            bot.send_message(message.chat.id, text='已解绑QAQ, 期待下次与你见面！', parse_mode='Markdown', timeout=20)
+        else:
+            bot.send_message(message.chat.id, text='你还没有绑定你解绑个啥？', parse_mode='Markdown', timeout=20)
     else:
         bot.send_message(message.chat.id, text='*⚠注意！您正在执行解除 Bangumi 账号绑定操作：\n\n解绑后您将无法继续使用：收藏管理，章节进度更新，发送评论，展示账户个人收藏，等一系列需要操作或读取账户功能。*\n\n如需继续解绑请输入 \\[`/unbind 确认`]\n', parse_mode='Markdown', timeout=20)
