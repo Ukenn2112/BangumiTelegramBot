07e117dc1c2ff38e15c83337b4b48304be24fec8
Ukenn <duola2002@gmail.com>
2022-10-18 15:22:41 +0900

    修改 netabare 评分分布获取方式

--- plugins/inline/mybgm.py
+++ plugins/inline/mybgm.py
@@ -6,13 +6,13 @@ from telebot.async_telebot import AsyncTeleBot
 from telebot.types import Message
 
 from config import APP_ID
-from utils.api import get_netabare_png, requests_get, get_user, user_data_get
+from utils.api import requests_get, get_user, user_data_get
 
 import config
-if 'CHROMEDRIVER_PATH' in dir(config):
-    CHROMEDRIVER_PATH = config.CHROMEDRIVER_PATH
+if 'WEBSTACK_API' in dir(config):
+    WEBSTACK_API = config.WEBSTACK_API
 else:
-    CHROMEDRIVER_PATH = ''
+    WEBSTACK_API = 'https://webstack-screenshot.vercel.app/'
 
 def query_mybgm_text(inline_query: Message, bot: AsyncTeleBot):
     message_data = inline_query.query.split(' ')
@@ -137,17 +137,18 @@ def query_mybgm_text(inline_query: Message, bot: AsyncTeleBot):
             thumb_url=img_url,
         )
         query_result_list.append(qr)
-        if CHROMEDRIVER_PATH:
-            netabare_img_url = get_netabare_png(bgm_id)
-            if netabare_img_url:
-                qr = telebot.types.InlineQueryResultPhoto(
-                    id=f'{inline_query.query}-netabare',
-                    photo_url=netabare_img_url,
-                    thumb_url=netabare_img_url,
-                    caption=f'_{nickname} 的 Bangumi 评分分布_',
-                    parse_mode="markdown",
-                )
-                query_result_list.append(qr)
+        if WEBSTACK_API:
+        #     netabare_img_url = get_netabare_png(bgm_id)
+        #     if netabare_img_url:
+            netabare_img_url = f"{WEBSTACK_API}?url=https://netaba.re/user/{bgm_id}&viewport=1728x968&waitUntil=networkidle0&await=320"
+            qr = telebot.types.InlineQueryResultPhoto(
+            id=f'{inline_query.query}-netabare',
+                photo_url=netabare_img_url,
+                thumb_url=netabare_img_url,
+                caption=f'_{nickname} 的 Bangumi 评分分布_',
+                parse_mode="markdown",
+            )
+            query_result_list.append(qr)
     except Exception:
         bot.answer_inline_query(
             inline_query.id,
