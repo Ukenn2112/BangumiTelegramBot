f5a30fcb80ce7f927156a9da52beccd3804eeb27
a <1452372270@qq.com>
2023-12-14 13:23:10 +0800

    修复在群组中 /book@BOT_USERNAME 等命令的使用

--- tgbot/collection_list.py
+++ tgbot/collection_list.py
@@ -1,30 +1,36 @@
 import uuid
+from typing import Literal,Dict,List
 
 from telebot.async_telebot import AsyncTeleBot
 from telebot.types import Message
 
 from utils.user_token import bgm_user_data
-
 from .model import CollectionsRequest, RequestSession, consumption_request
 
 
 async def send_collection_list(message: Message, bot: AsyncTeleBot):
     """处理用户收藏列表"""
     # 1: 书籍, 2: 动画 (默认), 3: 音乐, 4: 游戏, 6: 三次元
-    text_sp = message.text.split(" ")
-    subject_types = {
+    text_sp: List[str] = message.text.split(" ")
+    subject_types: Dict[str, Literal[1, 2, 3, 4, 6]] = {
         "/book": 1,
         "/anime": 2,
         "/music": 3,
         "/game": 4,
         "/real": 6,
     }
-    subject_type = subject_types.get(text_sp[0], 2)
+    subject_type: Literal[1, 2, 3, 4, 6] = 2
+    for key, value in subject_types.items():
+        if text_sp[0].lstrip().startswith(key):
+            subject_type = value
+            break
     user_data = await bgm_user_data(message.from_user.id)
     if user_data is None:
         bot_data = await bot.get_me()
-        return await bot.reply_to(message, f"发现您未绑定 Bangumi，快*[点我](https://t.me/{bot_data.username}?start=None)*进行绑定吧～", parse_mode="MarkdownV2")
-    msg = await bot.reply_to(message, "正在获取收藏列表，请稍后...")
+        return await bot.reply_to(message,
+                                  f"发现您未绑定 Bangumi，快*[点我](https://t.me/{bot_data.username}?start=None)*进行绑定吧～",
+                                  parse_mode="MarkdownV2")
+    msg = await bot.reply_to(message, "正在获取收藏列表，请稍候...")
     collection_type = 3
     if message.text:
         if len(text_sp) > 1:
@@ -46,6 +52,6 @@ async def send_collection_list(message: Message, bot: AsyncTeleBot):
     request = CollectionsRequest(
         session, subject_type=subject_type, collection_type=collection_type, user_bgm_data=user_data
     )
-    session.stack = [request] # 初始化栈
+    session.stack = [request]  # 初始化栈
     session.bot_message = msg
     await consumption_request(bot, session)
